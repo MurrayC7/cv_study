73e40ee767051bbd778054236a0959b54633131f
asus-cc <330784960@qq.com>
2018-10-11 21:32:32 +0800

    modify warp and generate some pics

--- warp.py
+++ warp.py
@@ -78,35 +78,42 @@ hsv[..., 1] = 255
 
 # while (1):
 ret, frame2 = cap.read()
-next = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+middle = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
-flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+ret, frame3 = cap.read()
+next = cv2.cvtColor(frame3, cv2.COLOR_BGR2GRAY)
 
-w = int(prvs.shape[1])
-h = int(prvs.shape[0])
-y_coords, x_coords = np.mgrid[0:h, 0:w]
-coords = np.float32(np.dstack([x_coords, y_coords]))
-pixel_map = coords + flow
+forward_flow = cv2.calcOpticalFlowFarneback(middle, prvs, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+backward_flow = cv2.calcOpticalFlowFarneback(middle, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+
+# w = int(prvs.shape[1])
+# h = int(prvs.shape[0])
+# y_coords, x_coords = np.mgrid[0:h, 0:w]
+# coords = np.float32(np.dstack([x_coords, y_coords]))
+# pixel_map = coords + flow
 
 
 # warped_image = cv2.remap(prvs, pixel_map, None, cv2.INTER_LINEAR)
-warped_image = warp_image(next, flow).astype(np.float32)
+warped_image_f = warp_image(middle, forward_flow).astype(np.float32)
+warped_image_b = warp_image(middle, backward_flow).astype(np.float32)
+warped_image = warped_image_b
 
 
-print("coords:",coords)
+# print("coords:",coords)
 # print("prev:",prvs)
 # print("next:",next)
 # print("warped_image:", warped_image)
-print(prvs.shape, next.shape, warped_image.shape, flow.shape, coords.shape)
-print(np.norm(warped_image - prvs) / np.norm(prvs))
+# print(prvs.shape, next.shape, warped_image.shape, flow.shape, coords.shape)
+print(np.sum(warped_image - prvs) / np.sum(prvs))
 
-mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-hsv[..., 0] = ang * 180 / np.pi / 2
-hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+# mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+# hsv[..., 0] = ang * 180 / np.pi / 2
+# hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+# rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
 cv2.imshow('frame1', prvs)
-cv2.imshow('frame2', next)
+cv2.imshow('frame2', middle)
+cv2.imshow('frame3', next)
 cv2.imshow('frame_warped', warped_image)
 k = cv2.waitKey(30) & 0xff
 # if k == 27:
@@ -115,9 +122,37 @@ k = cv2.waitKey(30) & 0xff
 #     cv2.imwrite('opticalfb.png', frame2)
 #     cv2.imwrite('opticalhsv.png', rgb)
 cv2.imwrite('frame1.png', prvs)
-cv2.imwrite('frame2.png', next)
+cv2.imwrite('frame2.png', middle)
+cv2.imwrite('frame3.png', next)
 cv2.imwrite('frame_warped.png', warped_image)
 # prvs = next
 
 cap.release()
 cv2.destroyAllWindows()
+
+sess = K.get_session()
+ 
+frozen_graph_def = tf.graph_util.convert_variables_to_constants(
+    sess,
+    sess.graph_def,
+    output_node_names=["y/Softmax"])
+ 
+# 保存图为pb文件
+tf.train.write_graph(frozen_graph_def, 'model', 'test_model.pb', as_text=False)
+
+
+with tf.Graph().as_default():
+    output_graph_def = tf.GraphDef()
+ 
+    with open('model/test_model.pb', "rb") as f:
+        output_graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(output_graph_def, name="")
+ 
+    with tf.Session() as sess:
+        init = tf.global_variables_initializer()
+        sess.run(init)
+ 
+        input_x = sess.graph.get_tensor_by_name("x_input:0")
+        output = sess.graph.get_tensor_by_name("y/Softmax:0")
+ 
+        print(sess.run(output, feed_dict={input_x: a.reshape(1, img_rows, img_cols, 1)})）
